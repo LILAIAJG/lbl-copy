99d285da5926e63641efc09d74406b36f0691780
LILAIAJG <LILAIAJG@163.com>
2025-01-19 16:38:18 +0800

    修改提示

---
+++ README.md
@@ -0,0 +1,11 @@
+### 这个工具可以让你在粘贴文本时模拟用户输入
+
+
+
+#### 一.安装依赖
+
+```
+pip install pyperclip keyboard
+```
+
+#### 二.运行main.py,按下shift + v即可
--- main.py
+++ main.py
@@ -14,5 +14,5 @@ def paste_clipboard_line_by_line():
 # 监听快捷键（例如 Ctrl+Shift+V）
 keyboard.add_hotkey('shift+v', paste_clipboard_line_by_line)
 
-print("按下 Ctrl+Shift+V 逐行粘贴剪贴板内容")
+print("按下 Shift+V 逐行粘贴剪贴板内容")
 keyboard.wait('esc')  # 按下 Esc 键退出程序
